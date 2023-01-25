b801005d24a86afcccf3f87e0a4254f5601bbdd7
Becka McNally <beckamcnally@gmail.com>
2023-01-24 20:26:18 -0800

    initial commit

---
+++ __tests__/engineer.test.js
@@ -0,0 +1,15 @@
+// do i need to require anything for these tests?? 
+
+//https://jestjs.io/docs/api#describename-fn
+
+// describe('what ever you type here is going to show as what you are testing in your console', () => {
+    // declare variables to use in the test
+//     test('human readable what is the test', () => {
+//       expect(what is the expected input).toBeTruthy(output)or equalTo();
+//     });
+//   });
+
+describe('Employee', () => {
+
+    test('')
+})
---
+++ __tests__/manager.test.js
@@ -0,0 +1,8 @@
+// prompted for manager’s name, employee ID, email address, and office number 
+
+// click on email address default email program populates the TO field of the email with the address
+
+// click github username and window open to github user
+
+// last prompt is option to add an engineer or an intern or to finish building my team
+    // if choose exit the application, and the HTML is generated
